68ab2707c4670596b757da4daa32f1afc2c785ea
UnleashedFighter <79016606+UnleashedFighter@users.noreply.github.com>
2026-01-05 21:16:30 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
